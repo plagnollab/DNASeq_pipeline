6c623234d5e883f54a5bd71ab3ae974eff4ec485
Vincent Plagnol <vincent.plagnol@gmail.com>
2014-11-29 23:45:45 +0000

    Basic commit

--- WGS/WGS_pipeline.sh
+++ WGS/WGS_pipeline.sh
@@ -32,76 +32,64 @@ inputFormat=STDFQ
 tparam=250
 
 
+####
+force=no
+enforceStrict=no
 
-filtDup=TRUE
-clean=TRUE
-saveMemory=yes
-paired=TRUE
-summaryStats=no
-fixUnique=no
+
+##### list of action parameters below
+align=no
 makegVCF=no
+makeVCF=no
 
-ispool="no"
-poolSAM=""
-poolnovoPile=""
+
+iFolder=""
+oFolder=aligned
 fasta="default.fasta"
 extraID=""
 
-align=no
+
 
 
 until [ -z "$1" ]; do
 	# use a case statement to test vars. we always test $1 and shift at the end of the for block.
     case $1 in
-	--inputFiles)
-	    shift
-	    i=0
-	    nfiles=$1
-	    inputFiles[ $i ]=$nfiles
-	    for f in `seq 1 $nfiles`; do 
-		shift
-		inputFiles[ $f ]=$1
-		echo $f $1
-	    done;;
 	--extraID )
 	    shift
 	    extraID="$1_";;
-	--filtDup )
-	    shift
-	    filtDup=$1;;
-	--fixUnique )
-	    shift
-	    fixUnique=$1;;
         --tempFolder )   ##specify a temp directory for the java picard code
-          shift
-          tempFolder=$1;;
-	--output )    ### critical to specify the output file
 	    shift
-	    output=$1;;
+	    tempFolder=$1;;
+	--supportFrame )    ### critical to specify the output file
+	    shift
+	    supportFrame=$1;;
 	--tparam )
 	    shift
 	    tparam=$1;;
 	--makegVCF )
 	    shift
 	    makegVCF=$1;;
+	--makeVCF )
+	    shift
+	    makeVCF=$1;;
 	--align)
 	    shift
 	    align=$1;;
 	--projectID)
 	    shift
 	    projectID=$1;;
-	--mainTable)
-	    shift
-	    mainTable=$1;;
-	--format)
-	    shift
-	    format=$1;;
         --reference)
             shift
             reference=$1;;
 	--fasta)
 	    shift
 	    fasta=$1;;
+	--force)
+	    shift
+	    force=$1;;
+	--enforceStrict)
+	    shift
+	    enforceStrict=$1;;
 	--inputFormat)
 	    shift
 	    inputFormat=$1;;
@@ -114,29 +102,13 @@ until [ -z "$1" ]; do
 done 
 
 
-if [[ "$tempFolder" != "" ]]; then
-    picardJavaTemp="TMP_DIR=$tempFolder"
-fi
-
-echo -e "
-Align: ${align} (t parameter is ${tparam})
-Annotate: $annotate
-Clean non required SAM/BAM files: $clean
-Input format: $inputFormat\n\n\n
-"
-
 
 extra="--rOQ --hdrhd 3 -H -k -a -o Soft -t ${tparam}"
 
-
 ########################### choice of reference sequence
 
-#if [ "$type" == RNA ]; then
-#    echo "Aligning RNA data"
-#    extra="-t ${tparam} -o Soft -a -v 20 0 200 \"[>]([^_]*)_\""
-#fi
 
-code=`basename $output`
+
 
 ############################### creates basic folders
 for folder in cluster cluster/out cluster/error cluster/submission; do
@@ -164,98 +136,135 @@ queue=queue6
 scratch=0
 
 if [[ "$summaryStats" == "yes" ]]; then ((nhours=nhours+2)); vmem=3; fi
+if [[ "$makeVCF" == "yes" ]]; then ((nhours=nhours+12)); vmem=6; memory=6; fi
 if [[ "$makegVCF" == "yes" ]]; then ((nhours=nhours+24)); vmem=6; memory=6; fi
 if [[ "$align" == "yes" ]]; then ((nhours=nhours+240)); ncores=12; vmem=1.3; memory=3; memory2=7; fi ##10 days?
 
 
+### running checks
+mustBeCode=`head -1 $supportFrame | cut -f1 -d' ' | cut -f1`  ##should accept tab or space as delimiters
+mustBeF1=`head -1 $supportFrame | cut -f2 -d' ' | cut -f2`
+mustBeF2=`head -1 $supportFrame | cut -f3 -d' ' | cut -f3`
 
-mainScript=cluster/submission/${projectID}_main.sh
-mainTable=cluster/submission/${projectID}_table.tab
-
-
-
+if [[ "$mustBeCode" != "code" ]]; then echo "The first column of the file $supportFrame must have the name code $mustBeCode"; exit; fi
+if [[ "$mustBeF1" != "f1" ]]; then echo "The second column of the file $supportFrame must have the name f1"; exit; fi
+if [[ "$mustBeF2" != "f2" ]]; then echo "The third column of the file $supportFrame must have the name f2"; exit; fi
 
 
 
 
 ########################### Now writing the script
-### I think I write and over-write this file many times (once per sample) but that's not a big deal
-echo "---------------------- Main script $mainScript"
-
-echo "
-#!/bin/bash
-#$ -S /bin/bash
-#$ -o cluster/out
-#$ -e cluster/error
-#$ -cwd" > $mainScript
-
-
-
-if [[ "$computer" == "CS" ]]; then
-    
-	echo "#$ -pe smp ${ncores}
-#$ -l scr=${scratch}G
-#$ -l tmem=${vmem}G,h_vmem=${vmem}G
-#$ -l h_rt=${nhours}:0:0
-#$ -tc 20
-#$ -V
-#$ -R y" >> $mainScript
-
-fi
-
-
-
 
 
 
 if [[ "$align" == "yes" ]]; then
 
-    script=`echo $mainScript | sed -e 's/.sh$//'`_${code}.sh
-    echo $script >> $mainTable
-    echo "
+    mainScript=cluster/submission/align.sh
+    mainTable=cluster/submission/align_table.sh
+    
+    echo "listScripts" > $mainTable
+
+    tail -n +2 $supportFrame | while read code f1 f2; do
+	
+	output=${oFolder}/${code}/${code}
+	script=`echo $mainScript | sed -e 's/.sh$//'`_${code}.sh
+
+
+	echo "
 ##start of script
 
 " > $script
+	
+	if [[ "$iFolder" != "" ]]; then
+	    f1=${iFolder}/${f1}
+	    f2=${iFolder}/${f2}
+	fi
+	
+	if [[ ! -s ${output}_sorted_unique.bam.bai || "$force" == "yes" ]]; then  ## proceed with that sample if force is set to yes or the output does not exist
+	    
+	    
+	    if [ ! -e $f1 ]; then echo "$f1 does not exist"; exit; fi
+	    if [ ! -e $f2 ]; then echo "$f2 does not exist"; exit; fi
+	    
+	    
+	    if [ ! -e ${tempFolder}/${code} ]; then mkdir ${tempFolder}/${code}; fi
+	    echo $script >> $mainTable
 
-    
-    if [ ! -e ${tempFolder}/${code} ]; then mkdir ${tempFolder}/${code}; fi
+	    
+	    echo "
 
-    echo "
+#$novoalign -c 11 -o SAM $'@RG\tID:${extraID}${code}\tSM:${extraID}${code}\tLB:${extraID}$code\tPL:ILLUMINA' $extra -F ${inputFormat} -f ${f1} ${f2}  -d $reference | ${samblaster} -e -d ${output}_disc.sam  | ${samtools} view -Sb - | $novosort - -t ${tempFolder} -c 8 -m ${memory2}G -i -o ${output}_sorted_unique.bam   ###this line is too ambitious, and I am now breaking it down into smaller pieces
 
-#$novoalign -c 11 -o SAM $'@RG\tID:${extraID}${code}\tSM:${extraID}${code}\tLB:${extraID}$code\tPL:ILLUMINA' $extra -F ${inputFormat} -f ${inputFiles[ 1 ]} ${inputFiles[ 2 ]}  -d $reference | ${samblaster} -e -d ${output}_disc.sam  | ${samtools} view -Sb - | $novosort - -t ${tempFolder} -c 8 -m ${memory2}G -i -o ${output}_sorted_unique.bam   ###this line is too ambitious, and I am now breaking it down into smaller pieces
-
-$novoalign -c 11 -o SAM $'@RG\tID:${extraID}${code}\tSM:${extraID}${code}\tLB:${extraID}$code\tPL:ILLUMINA' $extra -F ${inputFormat} -f ${inputFiles[ 1 ]} ${inputFiles[ 2 ]}  -d $reference | ${samblaster} -e -d ${output}_disc.sam  | ${samtools} view -Sb - > ${output}.bam
+$novoalign -c 11 -o SAM $'@RG\tID:${extraID}${code}\tSM:${extraID}${code}\tLB:${extraID}$code\tPL:ILLUMINA' $extra -F ${inputFormat} -f ${f1} ${f2}  -d $reference | ${samblaster} -e -d ${output}_disc.sam  | ${samtools} view -Sb - > ${output}.bam
 
 ${samtools} view -Sb ${output}_disc.sam | $novosort - -t ${tempFolder} -c 11 -m ${memory2}G -i -o ${output}_disc_sorted.bam
 
 $novosort -t ${tempFolder}/${code} -c 11 -m ${memory2}G -i -o ${output}_sorted_unique.bam ${output}.bam
 
 #rm ${output}_disc.sam ${output}.bam
+"  >> $script
+	    
+	    echo "$date" >> $script  ##to measure the duration
+	    echo $script
+	fi
+    done
 
+    #### compute the nb of jobs 
+    njobs=`wc -l $mainTable | cut -f1 -d' '`
+    ((njobs=njobs-1))
 
-" >> $script
+    ####### And now we work on the main script that combines them all
+    echo "
+#!/bin/bash
+#$ -S /bin/bash
+#$ -o cluster/out
+#$ -e cluster/error
+#$ -cwd
+#$ -pe smp ${ncores}
+#$ -l scr=${scratch}G
+#$ -l tmem=${vmem}G,h_vmem=${vmem}G
+#$ -l h_rt=${nhours}:0:0
+#$ -tc 20
+#$ -t 1-${njobs}
+#$ -V
+#$ -R y
 
-    echo "$date" >> $script  ##to measure the duration
-    echo $script
+array=( \`cat \"${mainTable}\" \`)
 
+script=\${array[ \$SGE_TASK_ID ]}
+
+echo \$script
+
+sh \$script
+
+" > $mainScript
+
+    
+
+    echo "Main submission scripts and tables for the align module:"
+    wc -l $mainScript $mainTable
 fi
 
 
 
 if [[ "$makegVCF" == "yes" ]]; then
-
-    for chrCode in `seq 1 25`;  do  ##one job per chromosome to save time
-
-	cleanChr=(targets 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y M )
+    
+    cleanChr=(targets 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y M )
+    
+    tail -n +2 $supportFrame | while read code f1 f2; do
 	
-	chrCleanCode=${cleanChr[ $chrCode ]}
-
-	if [ ! -s ${output}_chr${chrCleanCode}.gvcf.gz.tbi ]; then  ##if the index is not there, we assume that we have to do the whole job
-
-	    script=`echo $mainScript | sed -e 's/.sh$//'`_chr${chrCode}_${code}.sh
-	    echo $script >> $mainTable
+	output=${oFolder}/${code}/${code}
+	
+	for chrCode in `seq 1 25`;  do  ##one job per chromosome to save time
 	    
-	    echo "
+	    chrCleanCode=${cleanChr[ $chrCode ]}
+	    
+	    if [ ! -s ${output}_chr${chrCleanCode}.gvcf.gz.tbi | "$force" == "yes" ]; then  ##if the index is not there, we assume that we have to do the whole job
+		
+		script=`echo $mainScript | sed -e 's/.sh$//'`_chr${chrCode}_${code}.sh
+		echo $script >> $mainTable
+		
+		echo "
 $java17 -Djava.io.tmpdir=${tempFolder} -Xmx4g -jar $GATK -T HaplotypeCaller -R $fasta -I ${output}_sorted_unique.bam  \
        --emitRefConfidence GVCF --variant_index_type LINEAR --variant_index_parameter 128000 \
        -stand_call_conf 30.0 \
@@ -265,12 +274,82 @@ $java17 -Djava.io.tmpdir=${tempFolder} -Xmx4g -jar $GATK -T HaplotypeCaller -R $
        --GVCFGQBands 10 --GVCFGQBands 20 --GVCFGQBands 50 \
        -o ${output}_chr${chrCleanCode}.gvcf.gz
 " > $script
-
-	fi
+	    fi
+	done
     done
-
-    echo $script
+    
+  
 fi
 
+
+
+
+##############################################################################################################################
+
+if [[ "$makeVCF" == "yes" ]]; then
+    
+    mainScript=cluster/submission/makeVCF.sh
+    mainTable=cluster/submission/makeVCF_table.sh
+    echo "listScripts" > $mainTable
+
+    cleanChr=(targets 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y M )
+
+    for chrCode in `seq 1 25`;  do  ##one job per chromosome to save time
+	
+	chrCleanCode=${cleanChr[ $chrCode ]}
+	script=`echo $mainScript | sed -e 's/.sh$//'`_chr${chrCleanCode}.sh	
+	echo "$script" >> $mainTable
+
+	echo "
+$java17 -Xmx2g -jar $GATK \\
+   -R $fasta \\
+   -T GenotypeGVCFs \\
+   -L chr${chrCleanCode}  --interval_padding 100  \\
+   --annotation InbreedingCoeff --annotation QualByDepth --annotation HaplotypeScore --annotation MappingQualityRankSumTest --annotation ReadPosRankSumTest --annotation FisherStrand \\" > $script
+
+	tail -n +2 $supportFrame | while read code f1 f2; do  ### now look at each gVCF file
+	    output=${oFolder}/${code}/${code}
+	    gVCF="${output}_chr${chrCleanCode}.gvcf.gz"
+	    if [[ "$enforceStrict" == "yes" && ! -s $gVCF ]]; then echo "Cannot find $gVCF"; exit; fi
+	    if [ -s $gVCF ]; then 
+		echo "   --variant $gVCF \\" >> $script; 
+	    fi
+	done
+	
+	#echo "   --dbsnp ${bundle}/dbsnp_137.b37.vcf \\
+   #-o ${oFolder}/combined/combined_chr${chrCleanCode}.vcf.gz" >> $script
+
+	echo "   -o ${oFolder}/combined/combined_chr${chrCleanCode}.vcf.gz" >> $script
+	
+    done
+
+    echo "
+#!/bin/bash
+#$ -S /bin/bash
+#$ -o cluster/out
+#$ -e cluster/error
+#$ -cwd
+#$ -pe smp 1
+#$ -l scr=${scratch}G
+#$ -l tmem=4G,h_vmem=4G
+#$ -l h_rt=24:0:0
+#$ -tc 25
+#$ -t 1-25
+#$ -V
+#$ -R y
+
+array=( \`cat \"${mainTable}\" \`)
+
+script=\${array[ \$SGE_TASK_ID ]}
+
+echo \$script
+
+sh \$script
+
+" > $mainScript
+
+    echo "Main submission scripts and tables for the align module:"
+    wc -l $mainScript $mainTable
+fi
 
 
