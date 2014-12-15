b36fcde196fec37ce13d4d3af84dadf8ba8ff820
pontikos <npontikos@gmail.com>
2014-12-15 18:40:10 +0000

    allow for different fastq extensions in makesuppoort file

--- WGS/makesupportfile.sh
+++ WGS/makesupportfile.sh
@@ -1,7 +1,15 @@
-# *************************************************************
+# Takes as a command line argument the directory where the paired-end fastq files are.
+# Just redirect the output to the support file passed to the WGS pipeline.
+# How to run:
 # bash makesupportfile.sh <dir to fastq files>
-# *************************************************************
+ 
 # Directory where your paired end fq.gz lie.
 DIR=$1
+if [[ "$2" == "" ]]
+then
+    ext=fastq.gz
+else
+    ext=$2
+fi
 paste <(echo code) <(echo f1) <(echo f2)
-paste <(ls -1 $DIR/*_1.fq.gz | sed 's/_1.fq.gz//' | xargs -I f basename f) <(ls -1 $DIR/*_1.fq.gz) <(ls -1 $DIR/*_2.fq.gz)
+paste <(ls -1 $DIR/*_1.fastq.gz | sed "s/_1.${ext}//" | xargs -I f basename f) <(ls -1 $DIR/*_1.${ext}) <(ls -1 $DIR/*_2.${ext})
