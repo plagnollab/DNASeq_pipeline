c6fd5f160c9aed2dc1404fd91c64b53b6497a5ba
Nikolas Pontikos <npontikos@gmail.com>
2014-12-15 19:28:12 +0000

    simplification of arguments to script --mode [align|gvcf|jointvcf]

--- WGS/WGS_pipeline.sh
+++ WGS/WGS_pipeline.sh
@@ -8,162 +8,12 @@
 # 3) Jointly call VCFs using the GATK GenotypeGVCFs . This generates a multi-sample combined VCF file.
 
 
-# If you run this in a different environment than the UCL CS cluster
-# then you need to set the env variables.
-computer=CS
-if [[ "$computer" == "CS" ]]
-then
-    Software=/cluster/project8/vyp/vincent/Software
-    java=/share/apps/jdk1.7.0_45/jre/bin/java
-    bundle=/scratch2/vyp-scratch2/GATK_bundle
-    target=/cluster/project8/vyp/exome_sequencing_multisamples/target_region/data/merged_exome_target_cleaned.bed
-    tempFolder=/scratch2/vyp-scratch2/vincent/temp/novoalign
-fi
-
-### Tools needed by this script
-# Two functions of GATK will be used HaplotypeCaller and GenotypeGVCFs 
-GATK=${Software}/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar
-novoalign=${Software}/novocraft3/novoalign
-novosort=${Software}/novocraft3/novosort
-samblaster=${Software}/samblaster/samblaster
-##samtools
-samtools=${Software}/samtools-1.1/samtools
-## Picard
-picard=${Software}/picard-tools-1.100/
-picard_CreateSequenceDictionary=${picard}/CreateSequenceDictionary.jar 
-picard_MarkDuplicates=${picard}/MarkDuplicates.jar
-picard_CalculateHsMetric=${picard}/CalculateHsMetrics.jar
-picard_SamToFastq=${picard}/SamToFastq.jar
-
-############ default values
-#parameters to aligner
-inputFormat=STDFQ
-tparam=250
-
-####
-force=no
-enforceStrict=no
-
-##### list of action parameters below
-align=no
-makegVCF=no
-makeJointVCF=no
-
-# current default output folder is aligned
-oFolder=aligned
-# this is the default reference genome
-fasta="default.fasta"
-# this is used by the aligner but not sure what it does.  Vincent?
-extraID=""
-
-
-until [ -z "$1" ]
-do
-	# use a case statement to test vars. we always test $1 and shift at the end of the for block.
-    case $1 in
-	--extraID )
-	    shift
-	    extraID="$1_";;
-     --tempFolder )   ##specify a temp directory for the java picard code
-	    shift
-	    tempFolder=$1;;
-	--supportFrame )    ### critical to specify the output file
-	    shift
-	    supportFrame=$1;;
-	--tparam )
-	    shift
-	    tparam=$1;;
-# the main 3 steps of the program
-	--align)
-	    shift
-	    align=$1;;
-# Generate GVCF:
-	--makegVCF )
-	    shift
-	    makegVCF=$1;;
-# Do joint calling
-	--makeJointVCF )
-	    shift
-	    makeJointVCF=$1;;
-	--projectID)
-	    shift
-	    projectID=$1;;
-    --reference)
-        shift
-        reference=$1;;
-	--force)
-	    shift
-	    force=$1;;
-	--enforceStrict)
-	    shift
-	    enforceStrict=$1;;
-	--inputFormat)
-	    shift
-	    inputFormat=$1;;
-	-* )
-	    echo "Unrecognized option: $1"
-	    exit 1;;
-    esac
-    shift
-    if [ "$#" = "0" ]; then break; fi
-done 
-
-
-
-########################### supported reference sequences
-# all reference sequences and indexes should be under:
-# /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/
-fasta=none
-novoalignRef=none
-if [[ "$reference" == "hg38_noAlt" ]]
-then
-    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
-    novoalignRef=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.k15.s2.novoindex
-fi
-if [[ "$reference" == "1kg" ]]
-then
-    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta
-    novoalignRef=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta.k15.s2.novoindex
-fi
-if [[ "$reference" == "hg19" ]]
-then
-    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/hg19_UCSC.fa
-    novoalignRef=none
-fi
-
-############################### creates folders required for qsub and writing logs
-mkdir -p cluster cluster/out cluster/error cluster/submission
-
-###############  now let us check that the reference exists
-for file in $fasta
-do
-    ls -lh $file
-    if [ ! -e "$file"  ] && [ "$file" != "none" ]
-    then 
-        echo "Error, reference file $file does not exist"
-	exit
-    fi
-done
-
-
-###########################################################
-nhours=0
-ncores=1
-vmem=1
-memory2=5  ##used for the sort function, seem to crash when using 10
-queue=queue6
-scratch=0
-
-
-### Check format of support file.
-##should accept tab or space as delimiters
-## but does read support tabs and delimeters?
-mustBeCode=`head -1 $supportFrame | cut -f1 -d' ' | cut -f1`  
-mustBeF1=`head -1 $supportFrame | cut -f2 -d' ' | cut -f2`
-mustBeF2=`head -1 $supportFrame | cut -f3 -d' ' | cut -f3`
-if [[ "$mustBeCode" != "code" ]]; then echo "The first column of the file $supportFrame must have the name code $mustBeCode"; exit; fi
-if [[ "$mustBeF1" != "f1" ]]; then echo "The second column of the file $supportFrame must have the name f1"; exit; fi
-if [[ "$mustBeF2" != "f2" ]]; then echo "The third column of the file $supportFrame must have the name f2"; exit; fi
+function usage() {
+    echo "syntax: $0"
+    echo " --mode : [align|gvcf|jointvcf]"
+    echo " -h : prints this message"
+    exit 1
+}
 
 
 
@@ -191,7 +41,6 @@ function align() {
         script=`echo $mainScript | sed -e 's/.sh$//'`_${code}.sh
 	echo "
 ##start of script
-
 " > $script
     ## proceed with that sample if force is set to yes or the output does not exist
 	if [[ ! -s ${output}_sorted_unique.bam.bai || "$force" == "yes" ]]
@@ -202,9 +51,7 @@ function align() {
 	    echo $script >> $mainTable
 	    echo "
 $novoalign -c ${ncores} -o SAM $'@RG\tID:${extraID}${code}\tSM:${extraID}${code}\tLB:${extraID}$code\tPL:ILLUMINA' --rOQ --hdrhd 3 -H -k -a -o Soft -t ${tparam} -F ${inputFormat} -f ${f1} ${f2}  -d ${novoalignRef} | ${samblaster} -e -d ${output}_disc.sam  | ${samtools} view -Sb - > ${output}.bam
-
 ${samtools} view -Sb ${output}_disc.sam | $novosort - -t ${tempFolder} -c ${ncores} -m ${memory2}G -i -o ${output}_disc_sorted.bam
-
 $novosort -t ${tempFolder}/${code} -c ${ncores} -m ${memory2}G -i -o ${output}_sorted_unique.bam ${output}.bam
 #rm ${output}_disc.sam ${output}.bam
 "  >> $script
@@ -220,7 +67,7 @@ $novosort -t ${tempFolder}/${code} -c ${ncores} -m ${memory2}G -i -o ${output}_s
 # Instead of splitting by chromosome creates a single VCF file.
 # This is only manageable for exon and generally smaller sequence datasets.
 # This is currently not fully suported in the next step of doing joint-calling.
-function makeSingleGVCF() {
+function singlegvcf() {
     mainScript=cluster/submission/makesinglegVCF.sh
     mainTable=cluster/submission/makesinglegVCF_table.sh
     #start of while loop
@@ -253,7 +100,7 @@ function makeSingleGVCF() {
 ####################### GATK HaplotypeCaller split by chromosome  ############################################################
 # Take as input the sorted, unique BAM files and produces the gVCF files
 # Splits by chromosome.
-function makeGVCF() {
+function gvcf() {
     # GATK_HaplotypeCaller requires a sequence dictionary
     # submit as interactive long job?
     #
@@ -314,7 +161,7 @@ function makeGVCF() {
 ### This is the part that combines all the VCFs across samples to do the joint calling.
 ### This is a more practical aprroach of doing joint-calling than using the UnifiedGenotyper
 ### which relies on the BAM files.
-function makeJointVCF() {
+function jointvcf() {
     mainScript=cluster/submission/makeJointVCF.sh
     mainTable=cluster/submission/makeJointVCF_table.sh
     echo "listScripts" > $mainTable
@@ -359,10 +206,160 @@ $java -Xmx2g -jar $GATK \\
 }
 
 
-
 #################### the code below is generic to all modules: compute the nb of jobs and create the final submission array
 
-if [[ "$align" == "yes" ]]
+# If you run this in a different environment than the UCL CS cluster
+# then you need to set the env variables.
+computer=CS
+if [[ "$computer" == "CS" ]]
+then
+    Software=/cluster/project8/vyp/vincent/Software
+    java=/share/apps/jdk1.7.0_45/jre/bin/java
+    bundle=/scratch2/vyp-scratch2/GATK_bundle
+    target=/cluster/project8/vyp/exome_sequencing_multisamples/target_region/data/merged_exome_target_cleaned.bed
+    tempFolder=/scratch2/vyp-scratch2/vincent/temp/novoalign
+fi
+
+### Tools needed by this script
+# Two functions of GATK will be used HaplotypeCaller and GenotypeGVCFs 
+GATK=${Software}/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar
+novoalign=${Software}/novocraft3/novoalign
+novosort=${Software}/novocraft3/novosort
+samblaster=${Software}/samblaster/samblaster
+##samtools
+samtools=${Software}/samtools-1.1/samtools
+## Picard
+picard=${Software}/picard-tools-1.100/
+picard_CreateSequenceDictionary=${picard}/CreateSequenceDictionary.jar 
+picard_MarkDuplicates=${picard}/MarkDuplicates.jar
+picard_CalculateHsMetric=${picard}/CalculateHsMetrics.jar
+picard_SamToFastq=${picard}/SamToFastq.jar
+
+############ default values
+#parameters to aligner
+inputFormat=STDFQ
+tparam=250
+
+####
+force=no
+enforceStrict=no
+
+# current default output folder is aligned
+oFolder=aligned
+# this is the default reference genome
+fasta="default.fasta"
+# this is used by the aligner but not sure what it does.  Vincent?
+extraID=""
+
+
+until [ -z "$1" ]
+do
+	# use a case statement to test vars. we always test $1 and shift at the end of the for block.
+    case $1 in
+	--extraID )
+	    shift
+	    extraID="$1_";;
+     --tempFolder )   ##specify a temp directory for the java picard code
+	    shift
+	    tempFolder=$1;;
+	--supportFrame )    ### critical to specify the output file
+	    shift
+	    supportFrame=$1;;
+	--tparam )
+	    shift
+	    tparam=$1;;
+# the main 3 steps of the program
+# align
+# gvcf
+# jointvcf
+	--mode)
+	    shift
+	    mode=$1;;
+	--projectID)
+	    shift
+	    projectID=$1;;
+    --reference)
+        shift
+        reference=$1;;
+	--force)
+	    shift
+	    force=$1;;
+	--enforceStrict)
+	    shift
+	    enforceStrict=$1;;
+	--inputFormat)
+	    shift
+	    inputFormat=$1;;
+	-* )
+	    echo "Unrecognized option: $1"
+	    exit 1;;
+    esac
+    shift
+    if [ "$#" = "0" ]; then break; fi
+done 
+
+########################### supported reference sequences
+# all reference sequences and indexes should be under:
+# /scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/
+fasta=none
+novoalignRef=none
+if [[ "$reference" == "hg38_noAlt" ]]
+then
+    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+    novoalignRef=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.k15.s2.novoindex
+elif [[ "$reference" == "1kg" ]]
+then
+    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta
+    novoalignRef=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta.k15.s2.novoindex
+elif [[ "$reference" == "hg19" ]]
+then
+    fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/hg19_UCSC.fa
+    novoalignRef=none
+else
+    stop Unsupported reference $reference
+fi
+
+############################### creates folders required for qsub and writing logs
+mkdir -p cluster cluster/out cluster/err cluster/submission
+
+###############  now let us check that the reference exists
+for file in $fasta
+do
+    ls -lh $file
+    if [ ! -e "$file"  ] && [ "$file" != "none" ]
+    then 
+        echo "Error, reference file $file does not exist"
+	exit
+    fi
+done
+
+
+###########################################################
+nhours=0
+ncores=1
+vmem=1
+memory2=5  ##used for the sort function, seem to crash when using 10
+queue=queue6
+scratch=0
+
+
+### Check format of support file.
+##should accept tab or space as delimiters
+## but does read support tabs and delimeters?
+mustBeCode=`head -1 $supportFrame | cut -f1 -d' ' | cut -f1`  
+mustBeF1=`head -1 $supportFrame | cut -f2 -d' ' | cut -f2`
+mustBeF2=`head -1 $supportFrame | cut -f3 -d' ' | cut -f3`
+if [[ "$mustBeCode" != "code" ]]; then echo "The first column of the file $supportFrame must have the name code $mustBeCode"; exit; fi
+if [[ "$mustBeF1" != "f1" ]]; then echo "The second column of the file $supportFrame must have the name f1"; exit; fi
+if [[ "$mustBeF2" != "f2" ]]; then echo "The third column of the file $supportFrame must have the name f2"; exit; fi
+
+
+### program can run in 3 modes ###
+# align
+# makegVCF
+# makeJointVCF
+
+if [[ "align" == "$mode" ]]
 then
     ##10 days? Perhaps more.
     nhours=240
@@ -371,34 +368,39 @@ then
     memory2=7
     # call align function
     align
-fi
-if [[ "$makegVCF" == "yes" ]]
-then nhours=24
+#not fully supported yet
+elif [[ "singlegvcf" == "$mode" ]]
+then
+    nhours=24
     vmem=6
-    memory2=6
-    #
-    makeGVCF
-    #makeSingleGVCF
-fi
-if [[ "$makeJointVCF" == "yes" ]]
+    singlegvcf
+elif [[ "gvcf" == "$mode" ]]
+then
+    nhours=24
+    vmem=6
+    #memory2=6
+    gvcf
+#need to check with vincent
+elif [[ "jointvcf" == "$mode" ]]
 then
     nhours=12
     vmem=4
-    memory2=noneed
+    #memory2=noneed
     #
-    makeJointVCF
+    jointvcf
+else
+    stop "unknown mode: $mode"
 fi
 
-
+### The script to be submitted to qsub ###
 njobs=`wc -l $mainTable | cut -f1 -d' '`
 ((njobs=njobs-1))
-
 
 echo "
 #!/bin/bash
 #$ -S /bin/bash
 #$ -o cluster/out
-#$ -e cluster/error
+#$ -e cluster/err
 #$ -cwd
 #$ -pe smp ${ncores}
 #$ -l scr=${scratch}G
@@ -407,18 +409,12 @@ echo "
 #$ -tc 25
 #$ -t 1-${njobs}
 #$ -V
-#$ -R y
-
-array=( \`cat \"${mainTable}\" \`)
-
-script=\${array[ \$SGE_TASK_ID ]}
-
-echo \$script
-
+#$ -R y 
+array=( \`cat \"${mainTable}\" \`) 
+script=\${array[ \$SGE_TASK_ID ]} 
+echo \$script 
 sh \$script
-
 " > $mainScript
-
 
 echo "Main submission scripts and tables for the align module:"
 wc -l $mainScript $mainTable
