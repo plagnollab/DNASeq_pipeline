37d155031076710d03a0749f79aa3bdce98948bb
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-01-03 10:33:49 +0000

    Added the recalibration step on top of array based submission to the msample scripts

--- msample_calling/msample_calling.sh
+++ msample_calling/msample_calling.sh
@@ -8,6 +8,8 @@ try() { "$@" || stop "cannot $*"; }
 fasta=/scratch2/vyp-scratch2/reference_datasets/human_reference_sequence/human_g1k_v37.fasta
 bundle=/scratch2/vyp-scratch2/reference_datasets/GATK_bundle
 
+Rscript=/share/apps/R-3.1.0/bin/Rscript
+
 java=/share/apps/jdk1.7.0_45/bin/java
 tmpDir=/scratch0/vyp
 target=/cluster/project8/vyp/exome_sequencing_multisamples/target_region/data/merged_exome_target_cleaned.bed
@@ -17,9 +19,12 @@ GATK=/cluster/project8/vyp/vincent/Software/GenomeAnalysisTK-3.3-0/GenomeAnalysi
 submit=no
 
 force=no
-genotype=yes
+genotype=no
+recal=no
 gVCFlist=none
 
+
+maxGaussians=5
 
 #output=/scratch2/vyp-scratch2/vincent/GATK/cardioset_${currentUCLex}/cardioset_${currentUCLex}
 
@@ -38,6 +43,9 @@ until [ -z "$1" ]; do
 	--genotype )
 	    shift
 	    genotype=$1;;
+	--recal )
+	    shift
+	    recal=$1;;
 	--gVCFlist )
 	    shift
 	    gVCFlist=$1;;
@@ -67,28 +75,44 @@ mustBeId=`head -n1 $gVCFlist | cut -f2 -d' ' | cut -f2`
 if [[ "$mustBePath" != "path" ]]; then stop "The first column of the file $gVCFlist must have the name path $mustBePath"; fi
 if [[ "$mustBeId" != "id" ]]; then stop "The second column of the file $gVCFlist must have the name id $mustBeId"; fi
 
+mainScript=cluster/submission/calling.sh
+## individual scripts of the form cluster/submission/subcript_chr${chr}.sh
 
+echo "
+#$ -o cluster/out
+#$ -e cluster/error
+#$ -S /bin/bash
+#$ -l h_vmem=7.9G,tmem=7.9G
+#$ -l h_rt=72:0:0
+#$ -R y
+#$ -pe smp 1
+#$ -cwd 
+#$ -t 1-23
+#$ -tc 23
+
+LISTCHROMS=(chr 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X )
+
+CHR=\${LISTCHROMS[ \$SGE_TASK_ID ]}
+
+sh cluster/submission/subcript_chr\${CHR}.sh
+
+" > $mainScript
+
+
+######## first clean up the individual files
+for chr in `seq 1 22` X; do
+    if [ -e cluster/submission/subcript_chr${chr}.sh ]; then rm cluster/submission/subcript_chr${chr}.sh; fi
+done
+
+##################################################
 if [[ "$genotype" == "yes" ]]; then
     echo "Running the genotype module"
     
     for chr in `seq 1 22` X; do
+	script=cluster/submission/subscript_chr${chr}.sh
 	
-	echo "testing ${output}_chr${chr}.idx"
-	
-	if [ ! -e ${output}_chr${chr}.idx ]; then 
-	    
-	    script=cluster/submission/genotype_chr${chr}.sh
-	    
-	    echo "
-#$ -o cluster/out
-#$ -e cluster/error
-#$ -S /bin/bash
-#$ -l h_vmem=12.9G,tmem=12.9G
-#$ -l h_rt=72:0:0
-#$ -R y
-#$ -pe smp 1
-#$ -cwd " > $script
-	    
+	if [ ! -e ${output}_chr${chr}.vcf.gz.tbi ]; then 
+
 	    echo "
 
 $java -Xmx9g -jar $GATK \\
@@ -96,7 +120,7 @@ $java -Xmx9g -jar $GATK \\
    -T GenotypeGVCFs \\
    -L $chr -L $target --interval_set_rule INTERSECTION --interval_padding 100  \\
    --annotation InbreedingCoeff --annotation QualByDepth --annotation HaplotypeScore --annotation MappingQualityRankSumTest --annotation ReadPosRankSumTest --annotation FisherStrand \\
-   --dbsnp ${bundle}/dbsnp_137.b37.vcf \\" >> $script
+   --dbsnp ${bundle}/dbsnp_137.b37.vcf \\" >> cluster/submission/subcript_chr${chr}.sh
 	    
 	    
 	    while read path id; do
@@ -108,11 +132,85 @@ $java -Xmx9g -jar $GATK \\
 		
 		echo "   --variant $gVCF \\" >> $script
 	    done < <(tail -n +2 $gVCFlist)
-	    echo "   -o ${output}_chr${chr}" >> $script
-	    
-	    ls -ltrh $script
-	    qsub $script
+	    echo "   -o ${output}_chr${chr}.vcf.gz" >> $script
 	fi
     done 
     
 fi
+
+
+##################################################
+if [[ "$recal" == "yes" ]]; then
+    echo "Running the recalibration module"
+    
+    memoSmall=5
+
+    for chr in `seq 1 22` X; do
+	    
+	script=cluster/submission/subscript_chr${chr}.sh
+
+	#### creates the tmpDir if needed
+	tmpDir=/scratch0/GATK_chr${chr}
+	if [ ! -e $tmpDir ]; then mkdir $tmpDir; fi
+
+
+echo "
+####### first SNPs
+$java -Djava.io.tmpdir=${tmpDir} -Xmx${memoSmall}g -jar ${GATK} -T VariantRecalibrator -R $fasta --input ${output}_chr${chr}.vcf.gz --maxGaussians ${maxGaussians} --mode SNP \
+             -resource:hapmap,VCF,known=false,training=true,truth=true,prior=15.0 ${bundle}/hapmap_3.3.b37.vcf  \
+             -resource:omni,VCF,known=false,training=true,truth=false,prior=12.0 ${bundle}/1000G_omni2.5.b37.vcf \
+             -resource:dbsnp,VCF,known=true,training=false,truth=false,prior=8.0 ${bundle}/dbsnp_137.b37.vcf \
+             -an QD -an FS -an ReadPosRankSum -an InbreedingCoeff \
+             -tranche 100.0 -tranche 99.9 -tranche 99.8 -tranche 99.6 -tranche 99.5 -tranche 99.4 -tranche 99.3 -tranche 99.0 -tranche 98.0 -tranche 97.0 -tranche 90.0 \
+             --minNumBadVariants ${numBad} \
+             -recalFile ${output}_chr${chr}_SNP_combrec \
+             -tranchesFile ${output}_chr${chr}_SNP_combtranch \
+             -rscriptFile  ${output}_chr${chr}_recal_plots_snps.R
+
+${Rscript} ${output}_chr${chr}_recal_plots_snps.R
+
+
+#apply_recal
+$java -Xmx${memoSmall}g -jar ${GATK} -T ApplyRecalibration -R $fasta \
+       -o ${output}_chr${chr}_recal_SNP.vcf.gz \
+       --ts_filter_level 99.5 \
+       --recal_file ${output}_chr${chr}_SNP_combrec --tranches_file ${output}_chr${chr}_SNP_combtranch --mode SNP \
+       --input ${output}_chr${chr}.vcf.gz \
+
+
+
+######## now the indels
+$java -Djava.io.tmpdir=${tmpDir} -Xmx${memoSmall}g -jar ${GATK} -T VariantRecalibrator -R $fasta --input ${output}_chr${chr}_recal_SNP.vcf.gz --mode INDEL \
+           -resource:mills,known=true,training=true,truth=true,prior=12.0 ${bundle}/Mills_and_1000G_gold_standard.indels.b37.vcf \
+           -an QD -an FS -an ReadPosRankSum -an InbreedingCoeff \
+           -tranche 100.0 -tranche 99.5  -tranche 99.0 -tranche 97.0 -tranche 96.0 -tranche 95.0 -tranche 94.0 -tranche 93.0 -tranche 92.0 -tranche 91.0 -tranche 90.0 \
+           --minNumBadVariants ${numBadIndels} \
+           --maxGaussians ${maxGaussiansIndels} \
+           -recalFile ${output}_chr${chr}_INDEL_combrec \
+           -tranchesFile ${output}_chr${chr}_INDEL_combtranch \
+           -rscriptFile ${output}_chr${chr}_recal_plots_indels.R
+
+${Rscript} ${output}_chr${chr}_recal_plots_indels.R
+
+#apply_recal
+$java -Xmx${memoSmall}g -jar ${GATK} -T ApplyRecalibration -R $fasta \
+         --input ${output}_chr${chr}_recal_SNP.vcf.gz --out ${output}_chr${chr}_recal.vcf.gz \
+         --recal_file ${output}_chr${chr}_INDEL_combrec --tranches_file ${output}_chr${chr}_INDEL_combtranch --mode INDEL \
+         --ts_filter_level 95.0
+
+rm -rf $tmpDir
+
+" >> $script
+	
+
+    done
+    
+fi
+
+
+
+##############################
+for chr in `seq 1 22` X; do
+    ls -ltrh cluster/submission/subscript_chr${chr}.sh
+done
+wc -l $mainScript
