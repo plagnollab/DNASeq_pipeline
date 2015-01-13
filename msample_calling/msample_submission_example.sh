042de72a12e80095a81e867ba1dd8de9f25eed6c
Vincent Plagnol <vincent.plagnol@gmail.com>
2015-01-13 20:26:41 +0000

    Improved examples for msample calling

--- msample_calling/msample_submission_example.sh
+++ msample_calling/msample_submission_example.sh
@@ -1,8 +1,10 @@
 pipeline=/cluster/project8/vyp/vincent/Software/pipeline/msample_calling/msample_calling.sh
 
 
-genotype=no  #step1
-recal=yes  #step2
-annovar=yes ##step 3
+genotype=yes  #step1
+recal=no  #step2
+annovar=no ##step 3
+convertToR=no  ##step 4
 
-bash $pipeline --currentUCLex January2015 --gVCFlist support/gVCF.tab  --genotype ${genotype} --recal ${recal} --annovar ${annovar}
+bash $pipeline --currentUCLex January2015b --gVCFlist support/gVCF.tab  --genotype ${genotype} --recal ${recal} --annovar ${annovar} --convertToR ${convertToR}
+
