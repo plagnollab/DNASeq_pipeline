8ea422cac1e86374feab5dbafed5ccd6bc20d3f1
pontikos <npontikos@gmail.com>
2015-01-27 19:08:59 +0000

    https://github.com/vplagnol/pipelines/issues/21

--- annotation/liftOver.sh
+++ annotation/liftOver.sh
@@ -20,16 +20,25 @@ mkdir -p ${scriptname}.qsub.out ${scriptname}.qsub.err
 args=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y )
 ch=${args[$SGE_TASK_ID]}
 exec >${scriptname}.qsub.out/${scriptname}_chr${ch}_${JOB_ID}.out 2>${scriptname}.qsub.err/${scriptname}_chr${ch}_${JOB_ID}.err
-liftOver=/cluster/project8/vyp/AdamLevine/software/LiftOver/liftOver
-map=/cluster/project8/vyp/pontikos/hg19ToHg38.over.chain.gz 
 
 # ann is one of ExAC, esp etc
 ann=$1
 ann_dir=/cluster/project8/IBDAJE/VEP_custom_annotations/
+liftOver=/cluster/project8/vyp/AdamLevine/software/LiftOver/liftOver
+#map1=$ann_dir/chain_files/GRCh37ToHg19.over.chain.gz
+#map2=$ann_dir/chain_files/hg19ToHg38.over.chain.gz 
+map=$ann_dir/chain_files/GRCh37_to_GRCh38.chain.gz
 grc37=$ann_dir/GRCh37/$ann
 grc38=$ann_dir/GRCh38/$ann
 
-for vcfgz in $grc37/chr${ch}_*.vcf.gz
+#For UCLex and CADD we need this instead as we don't have any pop AF:
+if [[ "$ann" == "UCLex" ]] || [[ "$ann" == "CADD" ]] 
+then
+    files=$grc37/chr${ch}.vcf.gz
+else
+    files=$grc37/chr${ch}_*.vcf.gz
+fi
+for vcfgz in $files
 do
     [[ ! -s $vcfgz ]] && continue
     f=`basename ${vcfgz%.vcf.gz}`
@@ -39,16 +48,15 @@ do
     outvcf=$grc38/${f}.vcf
     header=$grc38/${f}.header
     outvcfgz=${outvcf}.gz
-    zcat $vcfgz | awk '{OFS="\t"; if (!/^#/){print $1,$2-1,$2,$3,$4,$5,$6}}' | sed 's/^/chr/' > $inbed
+    zcat $vcfgz | awk '{OFS="\t"; if (!/^#/){print $1,$2-1,$2,$3,$4,$5,$6}}'  > $inbed
+    # GRCh37 -> GRCh38
     $liftOver $inbed $map  $outbed $unliftedbed
     echo output `wc -l $outbed`
     echo unlifted `wc -l $unliftedbed`
     zgrep '^#' $vcfgz > $header
     #filter on chromosome name to remove positions mapped to different chrom or alternative sequences
-    sort -k 2 -n $outbed | sed 's/^chr//' | awk -v chr=$ch '{OFS="\t";if ($1==chr){print $1,$3,$4,$5,$6,".",".","."}}' > $outvcf.2
-    cat $header $outvcf.2 > $outvcf
-    rm $outvcf.2
-    bgzip -c -f $outvcf > $outvcfgz
+    cat $header <(sort -k 2 -n $outbed | awk -v chr=$ch '{OFS="\t";if ($1==chr){print $1,$3,$4,$5,$6,".",".","."}}') > $outvcf
+    bgzip -f -c $outvcf > $outvcfgz
     tabix -f -p vcf $outvcfgz
 done
 
