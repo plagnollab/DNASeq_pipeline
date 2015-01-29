bf8e6fed4f177f52857b295b588fd911afd4fcb1
pontikos <npontikos@gmail.com>
2015-01-29 22:21:04 +0000

    https://github.com/vplagnol/pipelines/issues/17#issuecomment-72090484

--- annotation/processVEP.py
+++ annotation/processVEP.py
@@ -12,10 +12,14 @@ EXAC=['EXAC_AFR', 'EXAC_AMR', 'EXAC_Adj', 'EXAC_EAS', 'EXAC_FIN', 'EXAC_NFE', 'E
 ONEKG=['1KG_EUR', '1KG_AFR', '1KG_AMR', '1KG_ASN', 'ESP_EA', 'ESP_AA']
 UCL=['UCLEX']
 
-ANNOTATION_HEADER=['VID']+CSQ+EXAC+UCL+['BF']
-annotation_file=open('variant_annotations.tab', 'w+')
-genotype_file=open('sample_genotypes.csv', 'w+')
-quality_file=open('sample_genotypes_quality.tab', 'w+')
+filename=sys.argv[1]
+infile=open(filename,'r')
+basename=filename.split('.')[0]
+
+ANNOTATION_HEADER=['VID']+CSQ+EXAC+UCL+['AF','WT','HET','HOM','MISS']
+annotation_file=open('-'.join([basename,'annotations.tab']), 'w+')
+genotype_file=open('-'.join([basename,'genotypes.csv']), 'w+')
+quality_file=open('-'.join([basename,'genotypes_depth.tab']), 'w+')
 for l in sys.stdin:
     #get the format of the VEP consequence (CSQ) field
     #the names of the VEP CSQ fields are "|" delimited
@@ -30,8 +34,8 @@ for l in sys.stdin:
         l=l.strip('#')
         HEADER=l.strip().split('\t')
         SAMPLES=HEADER[9:]
-        print(*(['VID']+[SAMPLES]),sep=',',file=genotype_file)
-        print(*(['VID']+[SAMPLES]),sep='\t',file=quality_file)
+        print(*(['VID']+SAMPLES),sep=',',file=genotype_file)
+        print(*(['VID']+SAMPLES),sep='\t',file=quality_file)
         print(*(ANNOTATION_HEADER),sep='\t',file=annotation_file)
         continue
     s=l.strip().split('\t')
@@ -97,8 +101,10 @@ for l in sys.stdin:
         cons=[ co for co in cons if co['Allele']==s['Allele'] ]
         for csq in CSQ_HEADER: s[csq] = ','.join([co[csq] for co in cons])
     # calculate freq of variant in this batch
-    #GENOTYPES.count(NA)
-    s['BF']=float(GENOTYPES.count(1)+GENOTYPES.count(2)*2) / len(SAMPLES)
+    s['MISS'] = float(GENOTYPES.count(NA)) / len(SAMPLES)
+    s['HET'] = float(GENOTYPES.count(1)) / len(SAMPLES)
+    s['HOM'] = float(GENOTYPES.count(2)) / len(SAMPLES)
+    s['AF']=float(GENOTYPES.count(1)+GENOTYPES.count(2)*2) / 2*len(SAMPLES)
     print(*([VARIANT_ID] + [s.get(h,'.')for h in ANNOTATION_HEADER]),sep='\t',file=annotation_file)
 
 
