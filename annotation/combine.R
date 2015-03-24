3beb31f667e737320d5661ef632192f417339203
pontikos <npontikos@gmail.com>
2015-03-24 17:12:33 +0000

    combines the three output files of processVEP: annotation, genotypes, genotype_depth

---
+++ annotation/combine.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+# Filtering of variants based on annotation 
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(tools))
+suppressPackageStartupMessages(library(xtable))
+
+option_list <- list(
+    make_option(c('--chr'), help='')
+)
+
+option.parser <- OptionParser(option_list=option_list)
+opt <- parse_args(option.parser)
+chr <- opt$chr
+ann <- read.csv(sprintf('VEP_%s-annotations.csv',chr))
+geno <- read.csv(sprintf('VEP_%s-genotypes.csv',chr))
+print(dim(geno <- geno[,-which('VARIANT_ID'==colnames(geno))]))
+colnames(geno) <- paste('geno',colnames(geno),sep='.')
+geno.depth <- read.csv(sprintf('VEP_%s-genotypes_depth.csv',chr))
+print(dim(geno.depth <- geno.depth[,-which('VARIANT_ID'==colnames(geno.depth))]))
+colnames(geno.depth) <- paste('depth',colnames(geno.depth),sep='.')
+
+d <- cbind(ann,geno,geno.depth)
+
+write.csv(d, quote=FALSE, row.names=FALSE, file=sprintf('VEP_%s.csv',chr))
+
+
