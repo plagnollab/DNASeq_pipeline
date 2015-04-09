7847297b7ce3eb2858a2fa23c57abd165cb8db07
pontikos <npontikos@gmail.com>
2015-04-09 12:00:08 +0100

    add annotation to VEP_${chr}.csv filfes

---
+++ annotation/annotate/annotate_expression.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+err.cat <- function(x)  cat(x, '\n', file=stderr())
+
+### 
+err.cat(dim( d <- read.csv(file('stdin')) ))
+
+
+# it is to use grep here I think because ENSG ids are fixed width (15 char)
+# so no risk of partial matching
+
+message('*** BARCODE COMMENT ***')
+barcode <- read.table('/cluster/project8/IBDAJE/barcode.txt', header=TRUE)
+barcode$Gene <- barcode$ENSG
+d$barcode.comment <- ''
+for (j in 1:nrow(barcode)) {
+    b <- barcode[j,]
+    d[grep(b['ENSG'],d$Gene),'barcode.comment'] <- b['COMMENT']
+
+}
+
+
+message('*** XAVIER TISSUES  ***')
+xavier <- read.csv('/goon2/scratch2/vyp-scratch2/reference_datasets/GeneEnrichmentProfiler/gene_tissues_ensg.csv')
+d$xavier.tissues <- ''
+for (j in 1:nrow(xavier)) {
+    b <- xavier[j,]
+    d[grep(b['gene'],d$Gene),'xavier.tissues'] <- b['tissues']
+
+}
+
+
+message('*** IBD_GENE_LIST ***')
+ibd.gene.list <- read.csv('/cluster/project8/IBDAJE/variants/IBD_GENE_LIST.txt',header=FALSE)
+d$ibd.gene.list <- FALSE
+for (j in 1:nrow(ibd.gene.list)) {
+    gene <- ibd.gene.list[j,1]
+    d[grep(gene,d$Gene),'ibd.gene.list'] <- TRUE
+}
+
+write.csv( d , quote=FALSE, file='', row.names=FALSE)
+
+
+
---
+++ annotation/annotate/annotate_pedigree_af.R
@@ -0,0 +1,62 @@
+#!/usr/bin/env Rscript
+
+err.cat <- function(x)  cat(x, '\n', file=stderr())
+
+### 
+#err.cat(dim( d <- read.csv(file('stdin')) ))
+
+samples <- gsub('geno\\.','',grep('geno',colnames(d),value=TRUE))
+
+pedigree <- 'DNA_pedigree_details.csv'
+
+# pedigree
+message('dim of pedigree')
+err.cat(nrow(pedigree <- read.csv(pedigree)))
+
+sample.affection <- pedigree[ which( pedigree$uclex.sample %in% samples ), c('uclex.sample','Affection')]
+# cases
+message('cases')
+err.cat(cases <- sample.affection[which(sample.affection$Affection==2),'uclex.sample'])
+message('number of cases')
+err.cat(length(cases))
+# controls
+message('controls')
+err.cat(controls <- sample.affection[which(sample.affection$Affection==1),'uclex.sample'])
+message('number of controls')
+err.cat(length(controls))
+
+
+calculate <- function(group, group.name){ #group: list of individuals of interest, group.name: subfamily name
+    #With geno. prefix
+    geno.group <- paste("geno",group,sep=".")
+    #Genotype columns
+    group.d.cols <- which(names(d) %in% geno.group)
+    #Genotypes
+    group.d.geno <- d[,group.d.cols]
+    #Number of WT, HET, HOM, MISS
+    group.wt <- apply(X=group.d.geno,1,function(X){length(which(X==0))})
+    group.het <- apply(X=group.d.geno,1,function(X){length(which(X==1))})
+    group.hom <- apply(X=group.d.geno,1,function(X){length(which(X==2))})
+    group.miss <- apply(X=group.d.geno,1,function(X){length(which(is.na(X)))})
+    #Allele frequency
+    group.af <- (group.het+group.hom*2)/(2*(group.wt+group.het+group.hom))
+    #Mutant frequency (percentage either HET or WT over total)
+    group.mf <- (group.het+group.hom)/(group.wt+group.het+group.hom)
+    #As data frame
+    group.out <- as.data.frame(cbind(group.wt,group.het,group.hom,group.miss,group.af,group.mf))
+    names(group.out) = paste(group.name,c("WT","HET","HOM","MISS","AF","MF"),sep="_")
+    return(group.out)
+
+}
+
+
+ca <- calculate(cases, 'cases')
+co <- calculate(controls, 'controls')
+d <- cbind(d,ca,co)
+
+write.csv( d , quote=FALSE, file='', row.names=FALSE)
+
+
+
+
+
