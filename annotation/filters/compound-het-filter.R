bdc5e69476d3ab5cde367a70849d6c4088f8b20b
Nikolas Pontikos <n.pontikos@gmail.com>
2015-09-17 00:34:44 +0100

    Update compound-het-filter.R

--- annotation/filters/compound-het-filter.R
+++ annotation/filters/compound-het-filter.R
@@ -1,8 +1,8 @@
 
 
 ped <- read.csv('pedigree_details.csv')
-ann <- read.csv('VEP_22-annotations.csv')
-geno <- read.csv('VEP_22-genotypes.csv')
+ann <- read.csv('VEP_21-annotations.csv')
+geno <- read.csv('VEP_21-genotypes.csv')
 
 mother <- na.omit(ped[,'Mother'])
 father <- na.omit(ped[,'Father'])
@@ -14,10 +14,17 @@ rbind( mother=table(x[,mother]=='0|1'), father=table(x[,father]=='0|1'), child=t
 
 
 by( geno , ann$SYMBOL, function(x) {
-
 x[,mother]=='0|1' & x[,father]=='0|1' & x[,affected]=='1|0'
-
 })
+
+X <- do.call('rbind',as.list(by( geno , ann$SYMBOL, function(x) {
+#rbind( mother=table(x[,mother]>0), father=table(x[,father]>0), child=table(x[,affected]>0))
+mum <- length(x[,mother]>0)
+dad <- length(x[,father]>0)
+child <- length(x[,affected]>0)
+return(child>mum&child>dad)
+})))
+
 
 
 # no crossovers?
