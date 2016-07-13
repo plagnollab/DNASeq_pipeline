d4ea50216bde26a39afa3ca950482012caddc1b3
pontikos <npontikos@gmail.com>
2016-07-14 00:41:16 +0100

    *.R

--- UCLex/convert_to_R.R
+++ UCLex/convert_to_R.R
@@ -1,37 +1,34 @@
-library(snpStats)
-library(GenomicRanges)
+#!/usr/bin/env Rscript
+err.cat <- function(x)  cat(x, '\n', file=stderr())
 
-#tstamp <- 'April2014'  ##813 cases, 436889 variants
-#tstamp <- Sys.getenv("currentUCLex")
-#if (tstamp == "") tstamp <- 'May2014'
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(tools))
+suppressPackageStartupMessages(library(xtable)) 
+suppressPackageStartupMessages(library(snpStats))
+suppressPackageStartupMessages(library(GenomicRanges))
 
-getArgs <- function() {
-  myargs.list <- strsplit(grep("=",gsub("--","",commandArgs()),value=TRUE),"=")
-  myargs <- lapply(myargs.list,function(x) x[2] )
-  names(myargs) <- lapply(myargs.list,function(x) x[1])
-  return (myargs)
-}
+### Series of filters suggested by Adam.
+# Filtering of variants based on annotation
 
+option_list <- list(
+    make_option(c('--root'), help='path to root'),
+    make_option(c('--chromosome'),  help='')
+) 
+option.parser <- OptionParser(option_list=option_list)
+opt <- parse_args(option.parser)
 
-myArgs <- getArgs()
-
-
-root <- '/scratch2/vyp-scratch2/vincent/GATK/mainset_October2014/mainset_October2014'
-if ('root' %in% names(myArgs)) root <- myArgs[[ 'root' ]]
-if ('chromosome' %in% names(myArgs)) chromosome <- as.character(myArgs[[ 'chromosome' ]])
-
+print(root <- opt$root)
+print(chromosome <- opt$chromosome)
 
 #######
-
 sample.names.file <- paste0(root, '_snpStats/colname_chr', chromosome, '.tab')
 sample.names <- scan(sample.names.file, what = character())
 
 ####### Now we read the data one chromosome at a time
 matrix.calls.snpStats.all <- NULL
 first <- TRUE
-
 message('Preparing data for chromosome ', chromosome)
-
 calls.file <- paste(root, '_snpStats/calls_chr', chromosome, '.tab', sep = '')
 depth.file <- paste(root, '_snpStats/depth_chr', chromosome, '.tab', sep = '')
 rowname.file <- paste(root, '_snpStats/rowname_chr', chromosome, '.tab', sep = '')
@@ -41,7 +38,7 @@ if (file.exists(rowname.file) && file.exists(calls.file)) {
   variant.names <- scan(rowname.file, what = character())
   n.calls <- length(variant.names)
   
-  annotations.snpStats <- read.csv(annotations.file, stringsAsFactors = FALSE)
+  annotations.snpStats <- as.data.frame(fread(annotations.file, stringsAsFactors = FALSE))
   annotations.snpStats$signature <- paste(annotations.snpStats$Chr, annotations.snpStats$Start, annotations.snpStats$Ref, annotations.snpStats$Obs, sep = '_')
   annotations.snpStats$clean.signature <- variant.names
   row.names(annotations.snpStats) <- annotations.snpStats$clean.signature
